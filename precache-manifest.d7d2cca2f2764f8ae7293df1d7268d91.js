0ce328384498202273b5662eb8eda035d3118a97
etoklim <vlad.klymenko@softermii.com>
2020-04-22 12:55:20 +0300

    Updates

---
+++ precache-manifest.d7d2cca2f2764f8ae7293df1d7268d91.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36351c5d1681e8254ca081f8cebe934",
+    "url": "/bookshelf/index.html"
+  },
+  {
+    "revision": "0cb714a2c1bf8c48e493",
+    "url": "/bookshelf/static/css/2.b992083f.chunk.css"
+  },
+  {
+    "revision": "97df60d32b4f2464534d",
+    "url": "/bookshelf/static/css/main.ad691205.chunk.css"
+  },
+  {
+    "revision": "0cb714a2c1bf8c48e493",
+    "url": "/bookshelf/static/js/2.5c3555eb.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/bookshelf/static/js/2.5c3555eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97df60d32b4f2464534d",
+    "url": "/bookshelf/static/js/main.5a4a8c4a.chunk.js"
+  },
+  {
+    "revision": "0a40412960a85694a131",
+    "url": "/bookshelf/static/js/runtime-main.91baa881.js"
+  },
+  {
+    "revision": "9cc83a59b0623c38281a7613721b25c1",
+    "url": "/bookshelf/static/media/austen_emma.9cc83a59.jpg"
+  },
+  {
+    "revision": "ead0b4339ea5705ca8a33ad1d2934880",
+    "url": "/bookshelf/static/media/austen_emma.ead0b433.epub"
+  },
+  {
+    "revision": "1c33207d04cbdee4093aef85a926f0b5",
+    "url": "/bookshelf/static/media/austen_pride.1c33207d.jpg"
+  },
+  {
+    "revision": "5236d2c143fd739617e966ad7aed6243",
+    "url": "/bookshelf/static/media/austen_pride.5236d2c1.epub"
+  },
+  {
+    "revision": "0661394196d0f04c466135a65486551d",
+    "url": "/bookshelf/static/media/carroll_alice.06613941.epub"
+  },
+  {
+    "revision": "af34d704dc2527b3fcf43f42dd396494",
+    "url": "/bookshelf/static/media/carroll_alice.af34d704.jpg"
+  },
+  {
+    "revision": "4746140d9aef45e7a1c2ca23f4d9a8e3",
+    "url": "/bookshelf/static/media/doyle_holmes.4746140d.jpg"
+  },
+  {
+    "revision": "acdd94dacbd8a529bac6498342601f1a",
+    "url": "/bookshelf/static/media/doyle_holmes.acdd94da.epub"
+  },
+  {
+    "revision": "6a8e5404d84cb033a4d6d46f931c4f48",
+    "url": "/bookshelf/static/media/london_call.6a8e5404.jpg"
+  },
+  {
+    "revision": "c6c2fa8cc229391a9df8d03618e451c4",
+    "url": "/bookshelf/static/media/london_call.c6c2fa8c.epub"
+  },
+  {
+    "revision": "1338075d4ed26754656829bb6bc4457f",
+    "url": "/bookshelf/static/media/shelley_frankenstein.1338075d.jpg"
+  },
+  {
+    "revision": "d3d2fe3299689f56244e571f5de7ca70",
+    "url": "/bookshelf/static/media/shelley_frankenstein.d3d2fe32.epub"
+  },
+  {
+    "revision": "1e63b391eb7e388f47b02874c2e029a8",
+    "url": "/bookshelf/static/media/stevenson_treasure.1e63b391.jpg"
+  },
+  {
+    "revision": "fc9f9760b2d36e7e567784f4a1d8abe9",
+    "url": "/bookshelf/static/media/stevenson_treasure.fc9f9760.epub"
+  },
+  {
+    "revision": "8bcd39f425bf3e0424a563ac020bd739",
+    "url": "/bookshelf/static/media/twain_finn.8bcd39f4.jpeg"
+  },
+  {
+    "revision": "fb303800451b4799a1ddea70b5dfec2e",
+    "url": "/bookshelf/static/media/twain_finn.fb303800.epub"
+  },
+  {
+    "revision": "beb44754c24e364033163f7473d7ee9f",
+    "url": "/bookshelf/static/media/twain_sawyer.beb44754.jpg"
+  },
+  {
+    "revision": "f558b10881102f6d10946c85467681bd",
+    "url": "/bookshelf/static/media/twain_sawyer.f558b108.epub"
+  }
+]);
